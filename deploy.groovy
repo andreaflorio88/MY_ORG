b761344e5ed2676957a52cfdfafe42fde514733f
Andrea Florio <58001390+andreaflorio88@users.noreply.github.com>
2024-11-05 11:45:12 +0100

    Update deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -47,8 +47,14 @@ pipeline {
                         """
                         
                         if(validation == 'true') {
+                            echo "Running validation..."
                             bat """
-                            sf apex test run --tests "${classes}" --wait 10 --json
+                                sf project deploy validate --target-org andreaflorio88@yahoo.it.new  --manifest "${manifestPath}" --wait 10 --verbose
+                            """
+
+                            echo "Running specified tests..."
+                            bat """
+                                sf apex test run --tests "${env.TESTS}" --wait 10 --json
                             """
                         } else {
                             bat """
