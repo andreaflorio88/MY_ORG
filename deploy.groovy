c74dff9d83ac1b818ff79f126644864f87e84967
Andrea Florio <58001390+andreaflorio88@users.noreply.github.com>
2024-11-05 11:10:04 +0100

    Update deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -53,7 +53,7 @@ pipeline {
                         """
                         if(validation) {
                             bat """
-                            sf project deploy validate --target-org andreaflorio88@yahoo.it.new --manifest ${manifestPath} --testlevel RunSpecifiedTests --runtests ${TESTS} --wait 10 --verbose
+                            sf project deploy validate --target-org andreaflorio88@yahoo.it.new --manifest ${manifestPath} --wait 10 --verbose
                             """
                         } else {
                             bat """
