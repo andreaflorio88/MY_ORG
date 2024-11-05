e11170e1d8ea0b6af527257e9d78940d142264d8
Andrea Florio <58001390+andreaflorio88@users.noreply.github.com>
2024-11-05 10:49:28 +0100

    Update deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -34,6 +34,8 @@ pipeline {
                         def environment = "${env.SF_ENV}"
                         def validation = "${env.SF_VALIDATION}"
 
+                        def check = 'start';
+
                         echo "${SF_ENV}"
                         echo "${PACKAGE}"
                         echo "${SF_VALIDATION}"
@@ -42,9 +44,12 @@ pipeline {
                         bat """
                         sfdx force:auth:jwt:grant --client-id 3MVG98Gq2O8Po4Zm6Dx8POjKJh1uGBbGl9QeBG7vEJDEl4JFgmOJJTDpXl3Lx8ksQpmDDsUt54xnXI_xBCsXk --jwt-key-file "%JWT_KEY%" --username andreaflorio88@yahoo.it.new --instance-url https://login.salesforce.com --set-default
                         """
+                        if(validation) {
+                            check = 'validate'
+                        }
                         
                         bat """
-                        sf project deploy start --target-org andreaflorio88@yahoo.it.new --manifest ${manifestPath} --wait 10 --verbose
+                        sf project deploy ${check} --target-org andreaflorio88@yahoo.it.new --manifest ${manifestPath} --wait 10 --verbose
                         """
                     }
                 }
