c2774ec446ba0c2f70e26e454f15ea394c60018f
Andrea Florio <58001390+andreaflorio88@users.noreply.github.com>
2024-11-05 12:02:03 +0100

    Update deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -54,7 +54,7 @@ pipeline {
                             
                             echo "Running validation..."
                             bat """
-                                sf project deploy validate --target-org andreaflorio88@yahoo.it.new  --manifest "${manifestPath}" --wait 10 --verbose
+                                sf project deploy validate --target-org andreaflorio88@yahoo.it.new --manifest ${manifestPath} --testlevel RunSpecifiedTests --runtests "AccountTriggerHandlerTest" --wait 10 --verbose
                             """
                         } else {
                             bat """
