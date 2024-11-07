79209b107891bd3d41ab0882f2a084debaf7d816
Andrea Florio <andreaflorio88@yhaoo.it>
2024-11-07 14:28:15 +0100

    ALIGN

--- align.groovy
+++ align.groovy
@@ -11,7 +11,7 @@ node {
     // -------------------------------------------------------------------------
 
     stage('Push to PARTIAL5') {
-        build job: 'Deploy - Dichiarativa', parameters: [booleanParam(name: 'Esegui i test', value: false), string(name: 'Ambiente di destinazione', value: 'PARTIAL5'), string(name: 'Package da utilizzare', value: PACKAGE)]
+        build job: 'Deploy - Dichiarativa', parameters: [booleanParam(name: 'Esegui i test', value: false), string(name: 'Ambiente di destinazione', value: 'PAR5'), string(name: 'Package da utilizzare', value: PACKAGE)]
     }
 
     // -------------------------------------------------------------------------
